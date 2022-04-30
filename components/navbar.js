a0e6f04859f1828615249c5c23bbd83d4aa80463
surajDongre-16 <surajdongre8@gmail.com>
2022-04-30 21:53:19 +0530

    react 1 assignment added

---
+++ components/navbar.js
@@ -0,0 +1,26 @@
+function navbar(){
+
+    return `
+    <div id="logo">
+        <img src="https://images.indianexpress.com/2021/02/AP-Airbnb-1200.jpg" alt="">
+        <h3>
+            <a href="#">airbnb</a>
+        </h3>
+    </div>
+    <div id="middle">
+        <h3><a href="#">Place to stay</a></h3>
+        <h3><a href="#">Experience</a></h3>
+        <h3><a href="#">Online Experience</a></h3>
+    </div>
+    <div id="options">
+        <h3>
+            <a href="#">Become a host</a>
+        </h3>
+        <h3>
+            <a href="signup.html">Signup</a>
+        </h3>
+    </div>
+    `
+}
+
+export {navbar}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import {navbar} from "../components/navbar"
+
+// console.log("navbar:",navbar)
+
+import "../styles/navbar.css"
+
+import "../styles/file.css"
+
+let navbar_div=document.getElementById("navbar")
+
+navbar_div.innerHTML=navbar()
+
